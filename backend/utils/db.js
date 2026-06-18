4b192f6d7de42014f23f66b4370a7d8958af5ad1
aryoshevar-123 <aryosheva.r@gmail.com>
2026-06-18 14:03:01 +0700

    refactor: remove console log everytime database connected

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -11,8 +11,6 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-pool.on('connect', () => {
-    console.log('PostgreSQL database connected successfully!');
-});
+pool.on('connect', () => {});
 
 export default pool;
