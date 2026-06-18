0476f5856dd374c1d8166c0b44ed96334741a86d
aryoshevar-123 <aryosheva.r@gmail.com>
2026-06-18 12:22:40 +0700

    chore: add export and import auth router

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 import pool from './utils/db.js';
 
+import authRoutes from './routes/authRoutes.js';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('Server is ready');
-});
-app.get('/api/test-db', async (req,res) => {
-    try {
-        const result = await pool.query('SELECT NOW() as current_time');
-
-        res.json({
-            status: "Success",
-            message: "Backend is successfully communication with PostgreSQL!",
-            db_time: result.rows[0].current_time
-        });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ status: "Error", error: "Database connection failed" });
-    }
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
