a4b40639e1076156c2147d5d8ed361d162609853
aryoshevar-123 <aryosheva.r@gmail.com>
2026-06-18 12:23:27 +0700

    fix: fix error on userExist not defined

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ export const registerUser = async (req,res) => {
     const { name, email, password, role, division_id } = req.body;
 
     try {
-        userExists = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const userExists = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (userExists.rows.length > 0) {
             return res.status(400).json({ message: 'Email already registered' });
         }
